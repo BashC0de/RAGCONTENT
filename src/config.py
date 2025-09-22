dffbd4cb6c3e3bbf306ae482e45db3938b69b871
BashC0de <afaqbook2@gmail.com>
2025-09-22 23:25:27 +0530

    Removed mirror folder from repo

--- src/config.py
+++ src/config.py
@@ -22,9 +22,10 @@ class Settings:
     WEAVIATE_URL = os.getenv("WEAVIATE_URL")
     WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 
-    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
     CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
     CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 settings = Settings()
 
+#removed the things and cleaned up imports
